9d91d1088374c0d08e5931b666fda4bac966e802
Joshua de Souza Ken <73433306+ShuaKen@users.noreply.github.com>
2024-07-28 20:27:23 -0300

    Add files via upload

---
+++ Teste/create-db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+banco = sqlite3.connect('TESTE_BD.db')
+
+cursor = banco.cursor()
+cursor.execute("CREATE TABLE Produtos(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, data TEXT NOT NULL, produto TEXT NOT NULL, tipo TEXT, unidade_embalagem TEXT, valor_unidade_de_medidas REAL, unidade_de_medidas TEXT, situacao_mercado TEXT, valor_min NUMERIC, valor_m_c_do_dia NUMERIC, valor_max NUMERIC, valor_variacao REAL, estados_siglas TEXT, cidade TEXT)")
+
+banco.commit()
+
+banco.close()
